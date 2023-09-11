afeeecf1527434eb9c49efa119ccaaea91c74358
Isaac Song <isaacsongs10@gmail.com>
2023-09-11 10:38:32 -0700

    Add function 2

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function distanceFromHqInBlocks(distance) {
     return Math.abs(distance - 42);
 }
 
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264;
+}
